46a1154442b8490cc20ed545dc456c484c082150
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-09 16:59:54 -0300

    Adicionando domínios que podem fazer fetch da minha api

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,14 @@ const express = require('express')
 const router = express.Router()
 const bp = require('body-parser')
 const posts = require('../model/posts')
+const cors = require('cors')
 
+const options = 
+{
+    origin: "http://localhost:3000"
+}
+
+router.use(cors(options)) //Se não colocar o parâmetro de options, habilita para que qualquer domínio possa fazer um fetch da minha API, com o parâmetro, apenas os domínios dentro da white list (options) podem fazer o fetch
 
 router.get('/all', (req, res) => {
     res.json(JSON.stringify(posts.getAll()))
